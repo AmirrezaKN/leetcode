a522043d7adb7ea50688e1afeff1c8835c734d02
Amirreza Kafshnochi <amirrezanochi@gmail.com>
2022-02-06 00:38:36 +0330

    - Solved 014

--- 014-longest-common-prefix/lcp.go
+++ 014-longest-common-prefix/lcp.go
@@ -1,5 +1,7 @@
 package longestcommonprefix
 
+import "strings"
+
 // Write a function to find the longest common prefix string amongst an array of strings.
 // If there is no common prefix, return an empty string "".
 
@@ -8,7 +10,16 @@ package longestcommonprefix
 // strs[i] consists of only lower-case English letters.
 
 func longestCommonPrefix(strs []string) string {
-	result := ""
+	result := strs[0]
+
+	for i := 1; i < len(strs); i++ {
+		for !strings.HasPrefix(strs[i], result) {
+			if result == "" {
+				return ""
+			}
+			result = result[:len(result)-1]
+		}
+	}
 
 	return result
 }
